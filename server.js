0252df7c3c68fcb38461e3f548a0e5005cb89ce6
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-11-22 10:02:48 -0300

    chore: configure MongoDB cluster, database, and collection; setup environment variables; connect database to API; add database config; refactor first GET

--- server.js
+++ server.js
@@ -1,41 +1,9 @@
 import express from "express";
-
-const posts = [
-  {
-    id: 1,
-    descricao: "Foto de teste do gato",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Foto de teste do gato preto",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Foto de teste do gato branco",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
   console.log("servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorID(req.params.id);
-  res.status(200).json(posts[index]);
 });
